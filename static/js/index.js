d17268734ee91db73723a5bfb03bd634753248c4
Corruption13 <sandeey999@gmail.com>
2019-07-20 20:09:35 +0530

    Added slideshow json implement

---
+++ static/js/index.js
@@ -0,0 +1,29 @@
+ function openNav() {
+    document.getElementById("mySidepanel").style.width = "25vw";
+
+  }
+
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+
+  }
+
+
+  var slideIndex = 0; // Initially set the index to 1
+  var slides = JSON.parse(slide);
+  var count = slides.length;
+
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+
+
+
+    if (n >= count) {slideIndex = 0}
+    if (n < 0) {slideIndex = count - 1}
+    $(".slides").html('<img class="slide_image" src="' + slides[slideIndex].location + '"> <div class=\"slide_textbox\">"' + slides[slideIndex].text + '"</div>');
+    //x[slideIndex-1].style.display = "block";
+
+  }
