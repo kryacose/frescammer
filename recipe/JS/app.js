871a1acec7deb52069f0b0e4ba68c40ac48d364e
Afnan-Navaz <afnannavaz@gmail.com>
2019-08-06 18:18:12 +0530

    recipe

--- recipe/JS/app.js
+++ recipe/JS/app.js
@@ -19,19 +19,6 @@ for (let i = 0; i < products.length; i += 3) {
     for (let j = i; j < i + 3; j++) {
         if (marker < len) {
 
-            /* checking the star rating using the rating from the data */
-            let rates = document.createElement('div');
-            rates.className = 'star';
-            for (let i = 0; i < 5; i++) {
-                let stars = document.createElement('span');
-                stars.classList.add('fa');
-                stars.classList.add('fa-star');
-                rates.appendChild(stars);
-            }
-            for (let k = 0; k < products[j].rating; k++) {
-                rates.childNodes[k].classList.add('checked');
-            }
-
             /* creating and appending the product cards into the DOM */
             let card = document.createElement('div');
             card.className = "card";
@@ -42,12 +29,8 @@ for (let i = 0; i < products.length; i += 3) {
             card.innerHTML += `<div class="prodEtail">
                                     <p class="proName">${products[j].name}</p>
                                     <p class="price">&#8377 <span style="font-size: 30px;">${products[j].price}</span></p>
-                                    <div class="star">
-                                    star rating
-                                    </div>
                                     <p style='display: none' class="disp">${products[j].description}</p>
                                 </div>`
-            card.childNodes[1].childNodes[5].innerHTML = rates.innerHTML;
 
             /* adding function for click response */
             card.addEventListener('click', function() {
@@ -55,8 +38,7 @@ for (let i = 0; i < products.length; i += 3) {
                 document.getElementById('prodImg-main').style.backgroundImage = cardData[0].style.backgroundImage;
                 document.getElementById('proName-main').innerText = cardData[1].childNodes[1].innerText;
                 document.getElementById('rate').innerText = cardData[1].childNodes[3].childNodes[1].innerText;
-                document.getElementById('star-main').innerHTML = cardData[1].childNodes[5].innerHTML;
-                document.getElementById('disp-main').innerText = cardData[1].childNodes[7].innerText;
+                document.getElementById('disp-main').innerText = cardData[1].childNodes[5].innerText;
                 mainCard.style.display = 'grid';
             })
             cardRow.appendChild(card);
