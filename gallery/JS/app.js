1423fb29f8aab89c248d3d0a99469c5af8de5d91
Afnan-Navaz <afnannavaz@gmail.com>
2019-08-06 22:52:57 +0530

    gallery update

---
+++ gallery/JS/app.js
@@ -0,0 +1,16 @@
+let dell = document.getElementById('close');
+let mainCard = document.getElementById('cardMain');
+let cards = document.getElementsByClassName('card');
+
+dell.addEventListener('click', () => {
+    mainCard.style.display = 'none';
+});
+
+console.log(cards.length);
+for(let i = 0; i < cards.length; i++){
+    cards[i].addEventListener('click', () => {
+        mainCard.style.backgroundImage = cards[i].childNodes[1].style.backgroundImage;
+        console.log(cards[i].childNodes[1].style.backgroundImage);
+        mainCard.style.display = 'block';
+    })
+}
